42a4b4d75933e0254341af9805aa2f7bd4a94dc6
Christopher Crnjak <christopher.crnjak@aol.de>
2024-02-07 10:35:19 +0100

    more structure

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function initAllThingsWeNeedToKnow() {
+    let myFirstVar = 0;
+    document.getby
+}
+
+let = tasks (
+    {
+        title: '',
+        description: '',
+        category: '',
+        contacts: {
+                    firstName: '',
+                    secondName: '',
+                    mail: '',
+                    },
+        dueDate: '',
+        priority: '',
+        subtasks: {
+                    task_1: '',
+                    task_2: '',
+                    },
+        status: '',
+
+    }
+)
