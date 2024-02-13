ebe342fcb99bb0e1d779445a568f49b08deddb1b
Christopher Crnjak <christopher.crnjak@aol.de>
2024-02-13 18:32:29 +0100

    worked on nextDeadline

--- js/summary.js
+++ js/summary.js
@@ -5,6 +5,8 @@ let awaitingFeedbackAmount = 0;
 let doneAmount = 0;
 let allAmounts = 0;
 let nextUpcomingTask; //TO-DO
+let allDeadlines  = [];
+let sortedDeadlines = [];
 
 async function init() {
     let resp = await fetch('assets/json/tasks.json'); 
@@ -13,6 +15,7 @@ async function init() {
     calcTaskAmount(tasks);
     calcSumOfAmounts();
     renderAmountsInElements();
+    calcNextDeadline();
 }
 
 function calcTaskAmount(tasks) {
@@ -91,3 +94,33 @@ function renderAwaitingFeedbackAmountInElements() {
         awaitingFeedbackAmountElement.innerHTML = "0";
     }
 }
+
+function calcNextDeadline() {
+    // for (let i = 0; i < tasks.length; i++) {
+    //     const task = tasks[i];
+    //     allDeadlines.push(task.dueDate);
+    //     sortedDeadlines.sort();
+    // }
+
+    // tasks.sort((a,b) => a.getTime() - b.getTime());
+
+
+}
+
+// Function to find the next upcoming due date
+function getNextDueDate(tasks) {
+    // Filter out tasks with due dates greater than today
+    const upcomingTasks = tasks.filter(task => new Date(task.dueDate) > new Date());
+  
+    // Sort the upcoming tasks by due date in ascending order
+    upcomingTasks.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate));
+  
+    // Return the due date of the first task (next upcoming due date)
+    return upcomingTasks.length > 0 ? upcomingTasks[0].dueDate : null;
+  }
+  
+  // Get the next upcoming due date
+  const nextDueDate = getNextDueDate(tasks);
+  
+  // Display the result
+  console.log("Next Upcoming Due Date:", nextDueDate);
