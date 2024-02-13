d915f5c656c30719309c6e9fca80e84960eecb4a
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-02-13 17:38:53 +0100

    finished render dialog board without clickable members and edit

--- js/board.js
+++ js/board.js
@@ -1,5 +1,6 @@
 let tasks = [];
 
+
 async function init() {
     let resp = await fetch('assets/json/tasks.json'); 
     tasks = await resp.json(); 
@@ -55,7 +56,7 @@ function noTask(toDo_container, inProgress_container, awaitFeedback_container, d
         inProgress_container.innerHTML = noTaskHTML('In Progress');
     };
     if (awaitFeedback_container.innerHTML == '') {
-        awaitFeedback_container = noTaskHTML('Await feedback');
+        awaitFeedback_container.innerHTML = noTaskHTML('Await feedback');
     };
     if (done_container.innerHTML == '') {
         done_container.innerHTML = noTaskHTML('done');
@@ -71,6 +72,7 @@ function renderTaskElements(i) {
     setColorOfCategory(i);
     showprogressbar(i);
     renderInitials(i);
+    renderPriority(i);
 }
 
 function setColorOfCategory(i) {
@@ -92,18 +94,34 @@ function taskHTML(task, i) {
     let title = task.title;
     let description = task.description;
     return `
-        <article id="task${i}" onclick="close_open_Dialog()" class="task">
+        <article id="task${i}" onclick="close_open_Dialog(${i})" class="task">
             <div id="task_category${i}" class="task_category">${category}</div>
             <div class="task_title">${title}</div>
             <div class="task_description">${description}</div>
             <div id="task_progressbar${i}" class="task_progress"></div>
             <div class="task_members_prio">
                 <div id="task_member_section${i}" class="task_members"></div>
-            <div class="task_prio">
+            <div id="prio_icon${i}" class="task_prio">
                 <img src="assets/img/Priority_symbols_Medium.png" alt="">
             </div>
         </article>
     `
+}
+
+function renderPriority(i) {
+    let priority = tasks[i].priority;
+    let container = document.getElementById(`prio_icon${i}`);
+    switch (priority) {
+        case "Low":
+          container.innerHTML = `<img src="assets/img/Priority_symbols_Low.png" alt="">`;
+          break;
+        case "Medium":
+            container.innerHTML = `<img src="assets/img/Priority_symbols_Medium.png" alt="">`;
+          break;
+        case "Urgent":
+          container.innerHTML = `<img src="assets/img/Priority_symbols_Urgent.png" alt="">`;
+        break;
+    }
 }
 
 function showprogressbar(i) {
@@ -199,9 +217,143 @@ function taskMemberHTML(firstCharacter, secondCharacter, colorClass, task_number
 
 // ****** DIALOG FUNCTIONS *****
 
-function close_open_Dialog() {
+
+
+function close_open_Dialog(taskId) {
     let container = document.getElementById('dialog_container');
-    container.classList.toggle('d-none')
+    container.classList.toggle('d-none');
+    if(!container.classList.contains('d-none')) {
+    renderDialogTask(taskId);}
+}
+
+ function renderDialogTask(taskId){
+    let container = document.getElementById('task_dialog_container');
+    let task = tasks[taskId];
+    container.innerHTML = taskDialogHTML(task, taskId);
+    setColorOfCategoryInDialog(taskId);
+    renderAssigedToDialog(taskId);
+    renderPriorityDialog(taskId);
+    renderSubtasksDialog(taskId);
+ }
+
+function taskDialogHTML(task, taskId) {
+    return`
+        <div class="category_close">
+                <div onclick="doNotClose(event)" id="task_category_dialog" class="task_category">${task.category}</div>
+                <a onclick="close_open_Dialog(${taskId})" class="close">
+                    <div class="line horizontal"></div>
+                    <div class="line vertical"></div>
+                </a>
+            </div>
+            <div onclick="doNotClose(event)" class="task_title_dialog">${task.title}</div>
+            <div onclick="doNotClose(event)" class="task_description_dialog">${task.description}</div>
+            <div onclick="doNotClose(event)" class="due_Date_dialog">
+                Due date: <span class="duedate">${task.dueDate}</span>
+            </div>
+            <div onclick="doNotClose(event)" class="priority_dialog">
+                Priority: <span >${task.priority}</span><div id="prio_image"></div>
+            </div>
+            <div onclick="doNotClose(event)" class="assigned_to">
+                <div>Assiged To:</div>
+                <div class="table_assigedto">
+                    <table>
+                        <tbody id="member_container_dialog"></tbody>
+                    </table>
+                </div>
+            </div>
+            <div onclick="doNotClose(event)" class="subtasks_main">
+                <div>Subtasks</div>
+                <div id="subtask_dialog_container" class="subtask_container">
+                </div>
+            </div>
+            <div onclick="doNotClose(event)" class="delete_and_edit_section">
+                <div class="delete_and_edit">
+                    <a onclick="deleteTask(${taskId})"><img src="assets/img/delete.png" alt="">Delete</a>
+                    <div class="line_task"></div>
+                    <a><img src="assets/img/edit.png" alt="">Edit</a>
+                </div>
+            </div>
+    `;
+}
+
+function setColorOfCategoryInDialog(i) {
+    let category = tasks[i].category;
+    container = document.getElementById('task_category_dialog');
+    switch (category) {
+        case 'User Story':
+          container.style.backgroundColor = '#0038FF';
+          break;
+        case 'Technical Task':
+          container.style.backgroundColor = '#1FD7C1';
+          break;
+    }
+}
+
+function renderAssigedToDialog(taskId) {
+    let container = document.getElementById('member_container_dialog');
+    container.innerHTML = '';
+    let task = tasks[taskId];
+    for (let i = 0; i < task.contacts.length; i++) {
+        let contact = task.contacts[i];
+        container.innerHTML += `
+            <tr>
+                <td class="member_cycle ${contact.color} pos1">${contact.firstName.charAt(0)}${contact.secondName.charAt(0)}</td>
+                <td class="member_name_assiged_to">${contact.firstName} ${contact.secondName}</td>
+            </tr>
+        `;
+    }
+}
+    function renderPriorityDialog(i) {
+        let priority = tasks[i].priority;
+        let container = document.getElementById('prio_image');
+        switch (priority) {
+            case "Low":
+              container.innerHTML = `<img src="assets/img/Priority_symbols_Low.png" alt="">`;
+              break;
+            case "Medium":
+                container.innerHTML = `<img src="assets/img/Priority_symbols_Medium.png" alt="">`;
+              break;
+            case "Urgent":
+              container.innerHTML = `<img src="assets/img/Priority_symbols_Urgent.png" alt="">`;
+            break;
+        }
+    }
+
+    function renderSubtasksDialog(i){
+        let container = document.getElementById('subtask_dialog_container');
+        container.innerHTML = '';
+        for (let j = 0; j < tasks[i].subtasks.length; j++) {
+            let subtask = tasks[i].subtasks[j];
+            container.innerHTML += `
+                <div class="Subtasks">
+                    <div id="subtask_status_img${i}${j}"></div>
+                    <span>${subtask.name}</span>
+                </div>
+            `;
+            renderSubtaskImage(i, j);
+        }
+    }
+
+    function renderSubtaskImage(taskId, subtaskId) {
+        let container = document.getElementById(`subtask_status_img${taskId}${subtaskId}`)
+        let status = tasks[taskId].subtasks[subtaskId].done;
+        switch (status) {
+            case true:
+              container.innerHTML = `<img src="assets/img/check_button_checked.png" alt="">`;
+              break;
+            case false:
+                container.innerHTML = `<img src="assets/img/check_button_unchecked.png" alt="">`;
+              break;
+        }
+    }
+
+
+{/* <img src="assets/img/check_button_checked.png" alt=""> */}
+
+function deleteTask(taskId) {
+    tasks.splice(taskId, 1);
+    renderColumnContent();
+    close_open_Dialog(taskId);
 }
 
 function doNotClose(event) {
