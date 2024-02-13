15ca0ccd8ba47c1d1e4d304aef592d369e74e5c8
Christopher Crnjak <christopher.crnjak@aol.de>
2024-02-13 08:33:30 +0100

    corrected render functions

--- js/summary.js
+++ js/summary.js
@@ -3,6 +3,7 @@ let toDoAmount = 0;
 let inProgressAmount = 0;
 let awaitingFeedbackAmount = 0;
 let doneAmount = 0;
+let allAmounts = 0;
 let nextUpcomingTask; //TO-DO
 
 async function init() {
@@ -57,17 +58,17 @@ function renderToDoAmountInElement() {
 
 function renderDoneAmountInElement() {
     let doneAmountElement = document.getElementById('doneAmount');
-    if (toDoAmount != 0) {
-        doneAmountElement.innerHTML = toDoAmount;
+    if (doneAmount != 0) {
+        doneAmountElement.innerHTML = doneAmount;
     } else {
         doneAmountElement.innerHTML = "0";
     }
 }
 
 function renderAllAmountInElement() {
-    let allAmountElement = document.getElementById('allAmount');
-    if (toDoAmount != 0) {
-        allAmountElement.innerHTML = toDoAmount;
+    let allAmountElement = document.getElementById('allAmounts');
+    if (allAmounts != 0) {
+        allAmountElement.innerHTML = allAmounts;
     } else {
         allAmountElement.innerHTML = "0";
     }
@@ -75,8 +76,8 @@ function renderAllAmountInElement() {
 
 function renderInProgressAmountInElement() {
     let inProgressAmountElement = document.getElementById('inProgressAmount');
-    if (toDoAmount != 0) {
-        inProgressAmountElement.innerHTML = toDoAmount;
+    if (inProgressAmount != 0) {
+        inProgressAmountElement.innerHTML = inProgressAmount;
     } else {
         inProgressAmountElement.innerHTML = "0";
     }
@@ -84,8 +85,8 @@ function renderInProgressAmountInElement() {
 
 function renderAwaitingFeedbackAmountInElements() {
     let awaitingFeedbackAmountElement = document.getElementById('awaitingFeedbackAmount');
-    if (toDoAmount != 0) {
-        awaitingFeedbackAmountElement.innerHTML = toDoAmount;
+    if (awaitingFeedbackAmount != 0) {
+        awaitingFeedbackAmountElement.innerHTML = awaitingFeedbackAmount;
     } else {
         awaitingFeedbackAmountElement.innerHTML = "0";
     }
