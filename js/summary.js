c6ec82f3371fa89903ef028dab54ec7d6a88f68c
Christopher Crnjak <christopher.crnjak@aol.de>
2024-02-14 16:17:28 +0100

    worked on getNextDueDate()

--- js/summary.js
+++ js/summary.js
@@ -11,11 +11,11 @@ let sortedDeadlines = [];
 async function init() {
     let resp = await fetch('assets/json/tasks.json'); 
     tasks = await resp.json(); 
-    console.log(tasks);
+    // console.log(tasks);
     calcTaskAmount(tasks);
     calcSumOfAmounts();
     renderAmountsInElements();
-    calcNextDeadline();
+    getNextDueDate();
 }
 
 function calcTaskAmount(tasks) {
@@ -95,45 +95,13 @@ function renderAwaitingFeedbackAmountInElements() {
     }
 }
 
-function calcNextDeadline() {
-    // for (let i = 0; i < tasks.length; i++) {
-    //     const task = tasks[i];
-    //     allDeadlines.push(task.dueDate);
-    //     sortedDeadlines.sort();
-    // }
-
-    // tasks.sort((a,b) => a.getTime() - b.getTime());
-
-
+function getNextDueDate() {
+    let tasksNotDone = tasks.filter((status) => {
+        return status.status != 'done';
+    })
+    console.log(tasksNotDone);
+    let dueDates = tasksNotDone.map((dueDates) => {
+        return dueDates.dueDate;
+    })
+    console.log(dueDates);
 }
-
-// Function to parse date string in "DD/MM/YYYY" format
-function parseDate(dateString) {
-    const [day, month, year] = dateString.split('/');
-    return new Date(`${year}-${month}-${day}`);
-  }
-  
-  // Function to find the next upcoming due date
-  function getNextDueDate(tasks) {
-    // Get the current date
-    const currentDate = new Date();
-  
-    // Filter out tasks with due dates less than or equal to the current date
-    const upcomingTasks = tasks.filter(task => {
-      const taskDate = parseDate(task.dueDate);
-      return taskDate > currentDate;
-    });
-  
-    // Sort the upcoming tasks by due date in ascending order
-    upcomingTasks.sort((a, b) => parseDate(a.dueDate) - parseDate(b.dueDate));
-  
-    // Return the due date of the first task (next upcoming due date)
-    return upcomingTasks.length > 0 ? upcomingTasks[0].dueDate : null;
-  }
-  
-  // Get the next upcoming due date
-  const nextDueDate = getNextDueDate(tasks);
-  
-  // Display the result
-  console.log("Next Upcoming Due Date:", nextDueDate);
-  
