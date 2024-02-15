113f48ecad7ca843e5cd56216175f2201c89b505
Christopher Crnjak <christopher.crnjak@aol.de>
2024-02-15 22:25:40 +0100

    created storage.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
-function handleLogin() {
+function handleLogIn() {
     console.log('test');
+    window.location.href ="summary.html"
+}
+
+function handleGuestLogIn() {
+    console.log('guesttest');
+    window.location.href ="summary.html"
 }
