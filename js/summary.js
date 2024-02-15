207219d79f779ca942e19f63def7d90f93fc8deb
Christopher Crnjak <christopher.crnjak@aol.de>
2024-02-15 14:45:43 +0100

    got next due date!

--- js/summary.js
+++ js/summary.js
@@ -96,17 +96,19 @@ function renderAwaitingFeedbackAmountInElements() {
 }
 
 function getNextDueDate() {
-    
+    // excludes the tasks which are done
     let tasksNotDone = tasks.filter((status) => {
         return status.status != 'done';
     })
     console.log('Tasks which are not done', tasksNotDone);
     
+    // create a new array called dueDates which only gives the dueDate-value of those tasks
     let dueDates = tasksNotDone.map((dueDates) => {
         return dueDates.dueDate;
     })
     console.log('Due dates of tasks which are not done', dueDates);
 
+    // changes the format of this string (DD/MM/YY to YY/MM/DD)
     let newArray = dueDates.map(dateString => {
         let [DD, MM, YY] = dateString.split('/');
 
@@ -117,6 +119,7 @@ function getNextDueDate() {
 
     console.log('Due dates in format YY/MM/DD', newArray);
 
+    // puts it into an date format and sort it from small to big
     let formatedDueDates = newArray.sort(function(a, b) {
         let dateA = new Date('20' + a.replace(/(\d{2})\/(\d{2})\/(\d{2})/, '$1-$2-$3'));
         let dateB = new Date('20' + b.replace(/(\d{2})\/(\d{2})\/(\d{2})/, '$1-$2-$3'));
@@ -126,6 +129,7 @@ function getNextDueDate() {
 
     console.log('Due dates in date format', formatedDueDates);
 
+    // outputs the first value of this array
     let firstValue = formatedDueDates[0];
     console.log(firstValue);
 }
