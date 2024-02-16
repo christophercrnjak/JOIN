12d86b680f1b61c70d096c46cdb706f756351a9b
Christopher Crnjak <christopher.crnjak@aol.de>
2024-02-16 16:13:24 +0100

    started to write signUp.js

---
+++ js/signUp.js
@@ -0,0 +1,26 @@
+let users = [];
+
+async function register() {
+    register_btn.disabled = true;
+    users.push({
+        email: email.value,
+        password: password.value
+    })
+
+    resetForm();
+}
+
+function resetForm() {
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let confirm_password = document.getElementById('confirm_password');
+    let checkbox = document.getElementById('checkbox');
+    let register_btn = document.getElementById('register_btn');
+    name.value = '';
+    email.value = '';
+    password.value = '';
+    confirm_password.value = '';
+    checkbox.checked = false;
+    register_btn.disabled = false;
+}
