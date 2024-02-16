c0b9e5bc8fba735c48609ab826fb3809aa7cc1a5
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-02-16 19:18:35 +0100

    set header comments

--- js/board.js
+++ js/board.js
@@ -1,18 +1,25 @@
+// fetched JSON in own array
 let tasks = [];
+
+// save the current dragged task as the Position-number in the array tasks
 let currentDraggedElement;
 
+// start on load the HTML page and saves the JSON content in the array tasks
+// initialized render of Kanban Board content
 async function init() {
     let resp = await fetch('assets/json/tasks.json'); 
     tasks = await resp.json(); 
-    console.log(tasks);
+    console.log(tasks); // only for control the JSON content during debugging
     renderColumnContent();
 }
 
+// The function is initialized by starting dragging a task
+// The parameter 'task' represents an index of the array 'tasks' and is transfered through rendering the html by functions taskHTML(task, i) -> i = param task
 function startDragging(task) {
     currentDraggedElement = task;
-    console.log(`${task}`)
 }
 
+// get Value of search-inputfield in lowerCase letters and trasfer function
 function searchTask() {
     let search_content = document.getElementById('task_to_be_found').value;
     search_content = search_content.toLowerCase();
@@ -29,32 +36,44 @@ function moveTo(status) {
 }
 
 
-// delete content of columns
+// render content of colums in depending on status
 async function renderColumnContent(search_content){
+    // Columns
     let toDo_container = document.getElementById('task_container_Todo');
     let inProgress_container = document.getElementById('task_container_InProgress');
     let awaitFeedback_container = document.getElementById('task_container_AwaitFeedback');
     let done_container = document.getElementById('task_container_Done');
+    // delet columns
+    deleteColumnContentToRenderNew(toDo_container, inProgress_container, awaitFeedback_container, done_container);
+    // sort by JSON content in associated Columns
+    await filterTasks(toDo_container, inProgress_container, awaitFeedback_container, done_container, search_content);
+    // render no task divs for columns without content/ not assigned status
+    noTask(toDo_container, inProgress_container, awaitFeedback_container, done_container);
+}
+
+// delete content of columns vor reset content
+function deleteColumnContentToRenderNew(toDo_container, inProgress_container, awaitFeedback_container, done_container) {
     toDo_container.innerHTML = '';
     inProgress_container.innerHTML = '';
     awaitFeedback_container.innerHTML = '';
     done_container.innerHTML = '';
-    await filterTasks(toDo_container, inProgress_container, awaitFeedback_container, done_container, search_content);
-    noTask(toDo_container, inProgress_container, awaitFeedback_container, done_container);
 }
 
-
+// sort by JSON content in associated Columns
 function filterTasks(toDo, inProgress, awaitFeedback, done, search_content) {
     for (let i = 0; i < tasks.length; i++) {
         let task = tasks[i];
-        switch (task.status) { // tasks[i].status has the Value of
+        switch (task.status) { // query tasks[i].status has the value of
 
-        case 'toDo': // case one tasks[i].status: 'toDo'
+        case 'toDo': // if value = 'toDo' true, 
+            // search filter and render if no searching, in dependance of searching title oder description
             if(!search_content || task.title.toLowerCase().includes(search_content) || task.description.toLowerCase().includes(search_content)) {
+            // render html of task
             toDo.innerHTML += taskHTML(task, i);
+            // render task special content
             renderTaskElements(i)}
             break;
-        case 'inProgress': // case one tasks[i].status: 'inProgress'
+        case 'inProgress': 
             if(!search_content || task.title.toLowerCase().includes(search_content) || task.description.toLowerCase().includes(search_content)) {
             inProgress.innerHTML += taskHTML(task, i);
             renderTaskElements(i)}
@@ -72,51 +91,6 @@ function filterTasks(toDo, inProgress, awaitFeedback, done, search_content) {
         }
     }
 }
-
-
-function noTask(toDo_container, inProgress_container, awaitFeedback_container, done_container) {
-    if (toDo_container.innerHTML == '') {
-        toDo_container.innerHTML = noTaskHTML('To do');
-    };
-    if (inProgress_container.innerHTML == '') {
-        inProgress_container.innerHTML = noTaskHTML('In Progress');
-    };
-    if (awaitFeedback_container.innerHTML == '') {
-        awaitFeedback_container.innerHTML = noTaskHTML('Await feedback');
-    };
-    if (done_container.innerHTML == '') {
-        done_container.innerHTML = noTaskHTML('done');
-    };
-}
-
-
-function noTaskHTML(head_text) {
-   
-    return `<div class="no_task"> no ${head_text} task</div>`
-}
-
-
-function renderTaskElements(i) {
-    setColorOfCategory(i);
-    showprogressbar(i);
-    renderInitials(i);
-    renderPriority(i);
-}
-
-
-function setColorOfCategory(i) {
-    let category = tasks[i].category;
-    container = document.getElementById(`task_category${i}`);
-    switch (category) {
-        case 'User Story':
-          container.style.backgroundColor = '#0038FF';
-          break;
-        case 'Technical Task':
-          container.style.backgroundColor = '#1FD7C1';
-          break;
-    }
-}
-
 
 function taskHTML(task, i) {
     let category = task.category;
@@ -138,6 +112,80 @@ function taskHTML(task, i) {
 }
 
 
+// render no task divs for columns without content/ not assigned status
+function noTask(toDo_container, inProgress_container, awaitFeedback_container, done_container) {
+    if (toDo_container.innerHTML == '') {
+        toDo_container.innerHTML = noTaskHTML('To do');
+    };
+    if (inProgress_container.innerHTML == '') {
+        inProgress_container.innerHTML = noTaskHTML('In Progress');
+    };
+    if (awaitFeedback_container.innerHTML == '') {
+        awaitFeedback_container.innerHTML = noTaskHTML('Await feedback');
+    };
+    if (done_container.innerHTML == '') {
+        done_container.innerHTML = noTaskHTML('done');
+    };
+}
+
+
+// HTML representation of no task div
+function noTaskHTML(head_text) {
+    return `<div class="no_task"> no ${head_text} task</div>`
+}
+
+
+// render task special content
+function renderTaskElements(i) {
+    setColorOfCategory(i); 
+    showprogressbar(i); // set blue bar
+    renderInitials(i); // Initials of task members
+    renderPriority(i); 
+}
+
+function setColorOfCategory(i) {
+    let category = tasks[i].category;
+    container = document.getElementById(`task_category${i}`);
+    switch (category) {
+        case 'User Story':
+          container.style.backgroundColor = '#0038FF';
+          break;
+        case 'Technical Task':
+          container.style.backgroundColor = '#1FD7C1';
+          break;
+    }
+}
+
+function showprogressbar(i) {
+    let container = document.getElementById(`task_progressbar${i}`)
+    let task = tasks[i];
+    if (task.subtasks.length > 0) {
+        container.innerHTML = subtaskHTML(i);
+        renderSubtaskAmounts(i);
+        renderBlueProgressbar(i)
+    } else {
+        container.classList.add('d-none')
+    }
+}
+
+function renderInitials(task_number) {
+    let task = tasks[task_number];
+    let container = document.getElementById(`task_member_section${task_number}`);
+    container.innerHTML = '';
+    for (let i = 0; i < task.contacts.length; i++) {
+        let contact = task.contacts[i];
+        let firstCharacter = contact.firstName.charAt(0);
+        let secondCharacter = contact.secondName.charAt(0);
+        let colorClass = contact.color;
+        container.innerHTML += taskMemberHTML(firstCharacter, secondCharacter, colorClass, task_number, i);   
+        if(i > 0) {
+            let additionalTaskMember = document.getElementById(`task_member${task_number}${i}`);
+            additionalTaskMember.style.position = 'relative';
+            additionalTaskMember.style.left     = calcPositionMember(i);
+        }
+    }
+}
+
 function renderPriority(i) {
     let priority = tasks[i].priority;
     let container = document.getElementById(`prio_icon${i}`);
@@ -151,19 +199,6 @@ function renderPriority(i) {
         case "Urgent":
           container.innerHTML = `<img src="assets/img/Priority_symbols_Urgent.png" alt="">`;
         break;
-    }
-}
-
-
-function showprogressbar(i) {
-    let container = document.getElementById(`task_progressbar${i}`)
-    let task = tasks[i];
-    if (task.subtasks.length > 0) {
-        container.innerHTML = subtaskHTML(i);
-        renderSubtaskAmounts(i);
-        renderBlueProgressbar(i)
-    } else {
-        container.classList.add('d-none')
     }
 }
 
@@ -220,25 +255,6 @@ function renderBlueProgressbar(i) {
 function calcProgressbar(totalSubtasks){
     let progressTotal = 128 / totalSubtasks;
     return +progressTotal;
-}
-
-
-function renderInitials(task_number) {
-    let task = tasks[task_number];
-    let container = document.getElementById(`task_member_section${task_number}`);
-    container.innerHTML = '';
-    for (let i = 0; i < task.contacts.length; i++) {
-        let contact = task.contacts[i];
-        let firstCharacter = contact.firstName.charAt(0);
-        let secondCharacter = contact.secondName.charAt(0);
-        let colorClass = contact.color;
-        container.innerHTML += taskMemberHTML(firstCharacter, secondCharacter, colorClass, task_number, i);   
-        if(i > 0) {
-            let additionalTaskMember = document.getElementById(`task_member${task_number}${i}`);
-            additionalTaskMember.style.position = 'relative';
-            additionalTaskMember.style.left     = calcPositionMember(i);
-        }
-    }
 }
 
 
@@ -379,14 +395,18 @@ function renderSubtasksDialog(i){
     container.innerHTML = '';
     for (let j = 0; j < tasks[i].subtasks.length; j++) {
         let subtask = tasks[i].subtasks[j];
-        container.innerHTML += `
-            <div class="Subtasks">
-                <div id="subtask_status_img${i}${j}"></div>
-                <span>${subtask.name}</span>
-            </div>
-        `;
+        container.innerHTML += subtaskDialogHTML(i, j, subtask.name);
         renderSubtaskImage(i, j);
     }
+}
+
+function subtaskDialogHTML(i, j, name) {
+    return `
+        <div class="Subtasks">
+            <div id="subtask_status_img${i}${j}"></div>
+            <span>${name}</span>
+        </div>
+    `;
 }
 
 
