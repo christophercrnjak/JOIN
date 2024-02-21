f856709ed0eb30ac04b63095dcc5923704deef09
Christopher Crnjak <christopher.crnjak@aol.de>
2024-02-21 20:01:06 +0100

    no changes

--- js/summary.js
+++ js/summary.js
@@ -196,5 +196,5 @@ function renderUserName() {
   let userNameElement = document.getElementById("user_name");
   let comma = document.getElementById("comma");
   userNameElement.innerHTML = userName;
-  comma.innerHTML += ",";
+  comma.innerHTML = ",";
 }
