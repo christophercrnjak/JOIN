600cdb3126a869fd8e18391ea5018033c246ebd6
Prizren89 <BeratKastrati1989@gmail.com>
2024-02-24 19:15:21 +0100

    Remove contacts

--- js/contact.js
+++ js/contact.js
@@ -12,7 +12,7 @@ function loadContacts() {
     let contactList = '';
     let displayedLetters = [];
 
-    for (let i = 0; i < person.length; i++) { // Iterieren Sie über 'person' statt 'tasks'
+    for (let i = 0; i < person.length; i++) {
         const persons = person[i];
         let firstLetter = persons.name.firstName.charAt(0).toUpperCase();
         let secondLetter = persons.name.secondName.charAt(0).toUpperCase();
@@ -34,8 +34,18 @@ function selectPerson(index) {
     selectedPersonElement.innerHTML = `
                                         <div class="persons_details">
                                             <div class="persons" style="background-color: ${selectedPerson.name.color};">${selectedPerson.name.firstName.charAt(0).toUpperCase()}${selectedPerson.name.secondName.charAt(0).toUpperCase()}</div>
-                                            <div class="person_details">${selectedPerson.name.firstName} ${selectedPerson.name.secondName}<br><div class="email">${selectedPerson.mail}</div></div>
-                                        </div>`;
+                                            <div class="person_details">${selectedPerson.name.firstName} ${selectedPerson.name.secondName}
+                                            <div class = "button_contacts"><button><img src= "./assets/img/edit.png"><h3>Edit</h3></button><button onclick="deletePerson(${index})"><img src= "./assets/img/delete.png"><h3>Delete</h3></button></div></div>
+                                            </div>
+                                            <div class = "info">
+                                            <h3>Contact Information</h3>
+                                            <h4>Email</h4>
+                                            <div class="emails">${selectedPerson.mail}</div>
+                                            <h4>Phone</h4>
+                                            <p>+49 1111 111 11 1</p>
+
+
+                                        `;
 }
 
 
@@ -58,4 +68,10 @@ function changeBackground(element) {
     setTimeout(function() {
         element.classList.add('active');
     }, 100);
+}
+function deletePerson(index) {
+    person.splice(index, 1);
+    const selectedPersonElement = document.getElementById('selectedPerson');
+    selectedPersonElement.innerHTML = '';
+    loadContacts(); 
 }
