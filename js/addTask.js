7a1adfc562e14331618cb6885dada5c21de39569
KruseKai <127634065+KruseKai@users.noreply.github.com>
2024-02-26 01:18:16 +0100

    updated addTask.js rework the Category.js section

--- js/addTask.js
+++ js/addTask.js
@@ -1,19 +1,24 @@
 let contacts = [];
+let categorys = ["Technical Task", "User Stroy"];
+
 let selectedFormDropdown = [];
 let prio = [];
-
+let pushCategory = [];
 
 function addTaskInit() {
   renderDropList();
+  renderCategoryDropDown();
 }
+
+// Selected contacts assign
 
 async function renderDropList() {
   // render the drop down menu form the
-  let response = await fetch('assets/json/contacts.json');
+  let response = await fetch("assets/json/contacts.json");
   let responseAsJson = await response.json();
-  let dropdown = document.getElementById('dropdown');
+  let dropdown = document.getElementById("dropdown");
 
-  dropdown.innerHTML = '';
+  dropdown.innerHTML = "";
   for (let i = 0; i < responseAsJson.length; i++) {
     let e = responseAsJson[i];
     dropdown.innerHTML += dropdownHtml(e);
@@ -34,28 +39,29 @@ function dropdownHtml(dropdownList) {
 function selectFromDropdown(e, imageUrl, firstName, secondName) {
   // Select for the Drop Down
   let isSelected = isItemSelected(imageUrl, firstName, secondName);
-  let dropdownList = document.getElementById('dropdownList');
+  let dropdownList = document.getElementById("dropdownList");
 
-  if (!isSelected) {
-    selectedFormDropdown.push({ imageUrl, firstName, secondName });
-    let imageElement = document.createElement('img');
-    imageElement.src = imageUrl;
-    dropdownList.appendChild(imageElement);
-    e.classList.add('selected');
-    imageElement.addEventListener('click', function () {
-      dropdownList.style.backgroundColor = '';
+  if (isSelected) {
+    // It another priority button is seleced, the other pervisously selected one will be changed
+    removeFromSelectedItems(firstName, secondName);
+    e.classList.remove("selected");
+
+    dropdownList.addEventListener("click", function () {
+      dropdownList.style.backgroundColor = "";
       dropdownList.removeChild(imageElement);
       removeFromSelectedItems(firstName, secondName);
-      e.classList.remove('selected');
     });
   } else {
-    removeFromSelectedItems(firstName, secondName);
-    e.classList.remove('selected');
-
-    dropdownList.addEventListener('click', function () {
-      dropdownList.style.backgroundColor = '';
+    selectedFormDropdown.push({ imageUrl, firstName, secondName });
+    let imageElement = document.createElement("img");
+    imageElement.src = imageUrl;
+    dropdownList.appendChild(imageElement);
+    e.classList.add("selected");
+    imageElement.addEventListener("click", function () {
+      dropdownList.style.backgroundColor = "";
       dropdownList.removeChild(imageElement);
       removeFromSelectedItems(firstName, secondName);
+      e.classList.remove("selected");
     });
   }
 }
@@ -77,71 +83,100 @@ function removeFromSelectedItems(firstName, secondName) {
   );
 }
 
-// Farbe änderung der btn urgrend, medium und low
+function filterFunction() {
+  // Filter on the drop Down menu
+  let input, filter, div, options, i, txtValue;
+  input = document.getElementById("dropdownInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("dropdown");
+  options = div.getElementsByClassName("dropdown_assign");
+  for (i = 0; i < options.length; i++) {
+    txtValue = options[i].textContent || options[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      options[i].style.display = "";
+    } else {
+      options[i].style.display = "none";
+    }
+  }
+}
+
+function toggledropbtn() {
+  let dropdownContent = document.getElementById("dropdown");
+
+  if (!dropdownContent.classList.contains("d-none")) {
+    dropdownContent.classList.toggle("show");
+    document.getElementById("dropdownInput").classList.toggle("d-none");
+    document.getElementById("dropbtn").classList.toggle("d-none");
+    document.getElementById("arrow").classList.toggle("rotated");
+  }
+}
+
+// Prio Btn
 
 function changePriority(priority) {
-  resetStyles(); // Setze alle Buttons und Bilder auf Standardwerte
-  removePreviousPriority(); // Vorherige Priorität aus dem Array löschen
+  resetStyles();
+  removePreviousPriority();
 
-  // Basierend auf der übergebenen Priorität die entsprechenden Änderungen vornehmen
-  switch (priority) {
-      case 'urgrend':
-          setPriorityStyles('#ff0000', '#ffff', '/assets/img/prio_ungrent.svg', 'urgrend');
-          break;
-      case 'medium':
-          setPriorityStyles('#ffa500', '#ffff', 'assets/img/prio_medium.svg', 'medium');
-          break;
-      case 'low':
-          setPriorityStyles('#008000', '#ffff', 'assets/img/prio_low.svg', 'low');
-          break;
-      default:
-          break;
+  if (priority === "urgrend") {
+    setPriorityStyles(
+      "#ff0000",
+      "#ffff",
+      "/assets/img/prio_ungrent.svg",
+      "urgrend"
+    );
+  } else if (priority === "medium") {
+    setPriorityStyles(
+      "#ffa500",
+      "#ffff",
+      "assets/img/prio_medium.svg",
+      "medium"
+    );
+  } else if (priority === "low") {
+    setPriorityStyles("#008000", "#ffff", "assets/img/prio_low.svg", "low");
   }
 }
 
 function resetStyles() {
-  let btnUrgrend = document.getElementById('btnUrgrend');
-  let btnMedium = document.getElementById('btnMedium');
-  let btnLow = document.getElementById('btnLow');
+  let btnUrgrend = document.getElementById("btnUrgrend");
+  let btnMedium = document.getElementById("btnMedium");
+  let btnLow = document.getElementById("btnLow");
 
-  let imgUrgrend = document.getElementById('btnUrgrendImg');
-  let imgMedium = document.getElementById('btnMediumImg');
-  let imgLow = document.getElementById('btnLowImg');
+  let imgUrgrend = document.getElementById("btnUrgrendImg");
+  let imgMedium = document.getElementById("btnMediumImg");
+  let imgLow = document.getElementById("btnLowImg");
 
   let buttons = [btnUrgrend, btnMedium, btnLow];
   let images = [imgUrgrend, imgMedium, imgLow];
 
-  buttons.forEach(function(button) {
-      button.style.backgroundColor = '#ffff';
-      button.style.color = 'black';
+  buttons.forEach(function (button) {
+    button.style.backgroundColor = "#ffff";
+    button.style.color = "black";
   });
 
-  images.forEach(function(image) {
-      switch (image.id) {
-          case 'btnUrgrendImg':
-              image.src = 'assets/img/Priority_symbols_Urgent.png';
-              break;
-          case 'btnMediumImg':
-              image.src = 'assets/img/Priority_symbols_Medium.png';
-              break;
-          case 'btnLowImg':
-              image.src = 'assets/img/Priority_symbols_Low.png';
-              break;
-          default:
-              break;
-      }
+  images.forEach(function (image) {
+    if (image.id === "btnUrgrendImg") {
+      image.src = "assets/img/Priority_symbols_Urgent.png";
+    } else if (image.id === "btnMediumImg") {
+      image.src = "assets/img/Priority_symbols_Medium.png";
+    } else if (image.id === "btnLowImg") {
+      image.src = "assets/img/Priority_symbols_Low.png";
+    }
   });
 }
 
 function removePreviousPriority() {
   if (prio.length > 0) {
-      prio.pop();
+    prio.pop();
   }
 }
 
 function setPriorityStyles(bgColor, textColor, imgSrc, priority) {
-  var button = document.getElementById('btn' + priority.charAt(0).toUpperCase() + priority.slice(1));
-  var image = document.getElementById('btn' + priority.charAt(0).toUpperCase() + priority.slice(1) + 'Img');
+  var button = document.getElementById(
+    "btn" + priority.charAt(0).toUpperCase() + priority.slice(1)
+  );
+  var image = document.getElementById(
+    "btn" + priority.charAt(0).toUpperCase() + priority.slice(1) + "Img"
+  );
 
   button.style.backgroundColor = bgColor;
   button.style.color = textColor;
@@ -149,32 +184,26 @@ function setPriorityStyles(bgColor, textColor, imgSrc, priority) {
 
   prio.push(priority);
 }
-
-function filterFunction() {
-  // Filter on the drop Down menu
-  let input, filter, div, options, i, txtValue;
-  input = document.getElementById('dropdownInput');
-  filter = input.value.toUpperCase();
-  div = document.getElementById('dropdown');
-  options = div.getElementsByClassName('dropdown_assign');
-  for (i = 0; i < options.length; i++) {
-    txtValue = options[i].textContent || options[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      options[i].style.display = '';
-    } else {
-      options[i].style.display = 'none';
-    }
-  }
+function categoryDropDownBtn() {
+  document.getElementById("dropdownCategory").classList.toggle("show");
 }
 
-function toggledropbtn() {
-  let dropdownContent = document.getElementById('dropdown');
-
-  if (!dropdownContent.classList.contains('d-none')) {
-    dropdownContent.classList.toggle('show');
-    document.getElementById('dropdownInput').classList.toggle('d-none');
-    document.getElementById('dropbtn').classList.toggle('d-none');
-    document.getElementById('arrow').classList.toggle('rotated');
+function renderCategoryDropDown() {
+  let dropdownCategory = document.getElementById("dropdownCategory");
+  dropdownCategory.innerHTML = "";
+  for (let i = 0; i < categorys.length; i++) {
+    let category = categorys[i];
+    dropdownCategory.innerHTML += `
+        <a class="dropdown_category" onclick="pushCategoryInTo('${category}')">${category}</a>
+        `;
+  }
+}
+function pushCategoryInTo(element) {
+  let category = document.getElementById("categoryDropDownBtn");
+  category.innerHTML = element;
+  pushCategory.push(element);
+  if (pushCategory.length > 1) {
+    pushCategory.shift();
   }
 }
 
@@ -185,5 +214,3 @@ function pushToJson() {
 function removeToJson() {
   // Remove the Add Task inputs in to a JSON
 }
-
-
