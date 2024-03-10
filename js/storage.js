d3d93be8d634612bd19b8e67448cd4f9e154c5f9
Richard Wezel <richard.wezel@posteo.de>
2024-03-11 00:52:41 +0100

    reset functions

--- js/storage.js
+++ js/storage.js
@@ -48,3 +48,204 @@ async function getTasksFromServer() {
 async function setTasksToServer() {
      setItem('tasks', tasks);   
 }
+
+/**
+ * Set the content of tasks.json file to Server.
+ */
+async function resetStorageOfServer_tasks() {
+  await setItem('tasks', [
+    {
+        "title": "Kochwelt Page & Recipe Recommender",
+        "description": "Build start page with recipe recommendation.",
+        "category": "User Story",
+        "contacts": [
+            {
+                "firstName": "Anton",
+                "secondName": "Mayer",
+                "color": "orange"
+            },
+            {
+                "firstName": "Emmanuel",
+                "secondName": "Mauer",
+                "color": "turquoise"
+            },
+            {
+                "firstName": "Marcel",
+                "secondName": "Bauer",
+                "color": "darkslateblue"
+            }
+        ],
+        "dueDate": "11/07/24",
+        "priority": "Medium",
+        "subtasks": [
+            {
+                "name": "Implement Recipe Recommendation",
+                "done": true
+            },
+            {
+                "name": "Start Page Layout",
+                "done": false
+            }
+        ],
+        "status": "inProgress"
+    },
+    {
+        "title": "HTML Base Template Creation",
+        "description": "Create reusable HTML base templates...",
+        "category": "Technical Task",
+        "contacts": [
+            {
+                "firstName": "David",
+                "secondName": "Eisenberg",
+                "color": "violet"
+            },
+            {
+                "firstName": "Benedict",
+                "secondName": "Ziegler",
+                "color": "mediumslateblue"
+            },
+            {
+                "firstName": "Anja",
+                "secondName": "Schulz",
+                "color": "blueviolet"
+            }
+        ],
+        "dueDate": "10/05/26",
+        "priority": "Low",
+        "subtasks": [],
+        "status": "awaitFeedback"
+    },
+    {
+        "title": "Daily Kochwelt Recipe",
+        "description": "Implement daily recipe and portion calculator....",
+        "category": "User Story",
+        "contacts": [
+            {
+                "firstName": "Eva",
+                "secondName": "Fischer",
+                "color": "yellow"
+            },
+            {
+                "firstName": "Anja",
+                "secondName": "Schulz",
+                "color": "blueviolet"
+            },
+            {
+                "firstName": "Tatjana",
+                "secondName": "Wolf",
+                "color": "red"
+            }
+        ],
+        "dueDate": "30/12/24",
+        "priority": "Medium",
+        "subtasks": [],
+        "status": "awaitFeedback"
+    },
+    {
+        "title": "CSS Architecture Planning",
+        "description": "Define CSS naming conventions and structure",
+        "category": "Technical Task",
+        "contacts": [
+            {
+                "firstName": "Sofia",
+                "secondName": "Müller",
+                "color": "deepskyblue"
+            },
+            {
+                "firstName": "Benedict",
+                "secondName": "Ziegler",
+                "color": "mediumslateblue"
+            }
+        ],
+        "dueDate": "01/07/25",
+        "priority": "Urgent",
+        "subtasks": [
+            {
+                "name": "Establish CSS Methodology",
+                "done": true
+            },
+            {
+                "name": "Setup Base Styles",
+                "done": true
+            }
+        ],
+        "status": "done"
+    }
+])
+}
+
+async function resetStorageOfServer_contacts() {
+  setItem('contacts', [
+    {
+      "name": {
+        "firstName": "Anton",
+        "secondName": "Mayer",
+        "color": "#FF7A00"
+      },
+      "mail": "anton@gmail.com",
+      "password": "",
+      "lockedIn": false
+    },
+    {
+      "name": {
+        "firstName": "Anja",
+        "secondName": "Schulz",
+        "color": "#9327FF"
+      },
+      "mail": "schulz@hotmail.de",
+      "password": "",
+      "lockedIn": true
+    },
+    {
+      "name": {
+        "firstName": "Benedikt",
+        "secondName": "Ziegler",
+        "color": "#6E52FF"
+      },
+      "mail": "benedikt@gmail.com",
+      "password": "",
+      "lockedIn": false
+    },
+    {
+      "name": {
+        "firstName": "David",
+        "secondName": "Eisenberg",
+        "color": "#FC71FF"
+      },
+      "mail": "davidberg@gmail.com",
+      "password": "",
+      "lockedIn": false
+    },
+    {
+      "name": {
+        "firstName": "Eva",
+        "secondName": "Fischer",
+        "color": "#FFBB2B"
+      },
+      "mail": "eva@gmail.com",
+      "password": "",
+      "lockedIn": false
+    },
+    {
+      "name": {
+        "firstName": "Emmanuel",
+        "secondName": "Mauer",
+        "color": "#1FD7C1"
+      },
+      "mail": "emmanuelma@gmail.com",
+      "password": "",
+      "lockedIn": false
+    },
+    {
+      "name": {
+        "firstName": "Marcel",
+        "secondName": "Bauer",
+        "color": "#462F8A"
+      },
+      "mail": "bauer@gmail.com",
+      "password": "",
+      "lockedIn": false
+    }
+  ]
+  );
+}
