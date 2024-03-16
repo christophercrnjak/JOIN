ccc418084ea482e356119aa63ac14ea98860a0cb
KruseKai <127634065+KruseKai@users.noreply.github.com>
2024-03-16 19:31:23 +0100

    js Changes for Sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,5 +1,14 @@
 
 function footerToggle() {
-    const footerNotice = document.getElementById("footer_notice");
-    footerNotice.classList.toggle("show");
+  let footerNotice = document.getElementById('footer_notice');
+  footerNotice.classList.toggle('show');
+}
+
+document.addEventListener('click', function(event) {
+  let footerNotice = document.getElementById('footer_notice');
+  if (!event.target.closest('.footer_notice') && !footerNotice.contains(event.target)) {
+    footerNotice.classList.remove('show');
   }
+});
+  
+  
