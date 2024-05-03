db3e485c6678bb42ee308632f512a79e8ff3f7a1
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-05-03 19:53:49 +0200

    added js docs to navigation js

--- js/navigation.js
+++ js/navigation.js
@@ -1,6 +1,13 @@
+/**
+ * Link to the last side visited before.
+ */
 function back() {
     window.history.back();
 }
+
+/**
+ * Link to the main contact list in the mobile version.
+ */
 function backMobile() {
     if (window.innerWidth <= 1000) {
         window.location.href = "contacts.html";
